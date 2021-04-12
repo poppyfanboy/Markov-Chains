68a701011573cd8badc0fbf1bd24bb910894b1d6
poppyfanboy <poppyfanboy69@gmail.com>
2021-04-13 00:33:01 +0300

    Remove non-essential deps at deploy time

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import './css/index.pcss';
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import MarkovChainsApp from './components/markov-chains-app/MarkovChainsApp';
+import MarkovChainsApp from '@components/markov-chains-app/MarkovChainsApp';
 
 ReactDom.render(
     <MarkovChainsApp />,
